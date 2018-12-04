bf181638f7ca857ee99d5bb3652f9427525d6c74
Peng Zhao <pzhao@pzhao.net>
2018-12-04 22:11:37 +0100

    update auto

--- static/slides/ross-mpic/macro/macro.js
+++ static/slides/ross-mpic/macro/macro.js
@@ -1,3 +1,9 @@
+/* This is a self-defined macro, created by Peng Zhao <pzhao@pzhao.net>. */
+/* Anyone can re-use or modify this file for free. */
+/* Please leave a trace here if you modify it. */
+/* 2018-12-02 Peng Zhao: created. */
+/* 2018-12-04 Peng Zhao: uploaded and shared. */
+
 remark.macros.width = function (width) {
   var url = this;
   return '<img src="' + url + '" style="width: ' + width + '" />';
