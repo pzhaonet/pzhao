e2dc18b8d6f719c08e8c8cb34325068a12d4e2da
Peng Zhao <pzhao@pzhao.net>
2021-11-20 14:50:35 +0800

    mindr

---
+++ content/talk/2021-08-12-interview-xjtlu.en.md
@@ -0,0 +1,24 @@
++++
+date = "2021-08-12T00:00:00"
+title = "XJTLU Interview：XJTLU staff publishes book on modern statistical graphics"
+abstract = ""
+abstract_short = ""
+event = "Interview"
+event_url = "https://www.xjtlu.edu.cn/en/news/2021/09/newbookpublishedbydrpengzhao"
+location = ""
+
+selected = false
+math = true
+
+url_slides = ""
+url_video = ""
+
+# Optional featured image (relative to `static/img/` folder).
+
+[header]
+image = ""
+caption = "My caption :smile:"
+
++++
+
+![](https://www.xjtlu.edu.cn/assets/images/department_resource/science/news/2021/08/WechatIMG95.jpeg)
---
+++ static/talk/mindr/img/eq.mm
@@ -0,0 +1,65 @@
+<map version="docear 1.1" dcr_id="1637327414782_apv2gbbunqpfyl8y6g9r5whyi" project="17D287DBD9AE6KSZEAYJBVMX9SCWC725XQ3S" project_last_home="file:/C:/Users/dapen/Docear/projects/My%20Thesis">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="t test" FOLDED="false" ID="ID_1308812810" CREATED="1637327412511" MODIFIED="1637327412511"><hook NAME="MapStyle" zoom="1.61">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="One-sample " POSITION="right" ID="ID_327426841" CREATED="1637327412511" MODIFIED="1637327412511">
+<hook EQUATION="t =\frac{\bar x - \mu}{s / \sqrt{n}}" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="Two-sample" POSITION="right" ID="ID_1962279315" CREATED="1637327412511" MODIFIED="1637327412511">
+<node TEXT="With equal variance " ID="ID_1445468541" CREATED="1637327412511" MODIFIED="1637327412511">
+<hook EQUATION="t =  \frac{(\bar x_1 - \bar x_2) - (\mu_1 - \mu_2)}{\sqrt{\frac{(n-1)s_1^2 + (m - 1)s_2^2}{(n - 1) + (m -1)} (\frac{1}{n} + \frac{1}{m})}}" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="With unequal varicances" ID="ID_1245896659" CREATED="1637327419411" MODIFIED="1637327528639">
+<hook EQUATION="t = \frac{\bar x_d - \mu_d}{\sqrt{\frac{s_1^2}{n} + \frac{s_2^2}{m}}}" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+</node>
+</node>
+</map>
